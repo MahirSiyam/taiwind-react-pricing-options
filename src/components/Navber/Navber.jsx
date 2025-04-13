fcf84ab06d8c8aa29a4498416311c8bab77cb233
Mahir Siyam <mahirmubtasimsiyam@gmail.com>
2025-04-13 12:17:20 +0600

    added navber

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,46 +1,49 @@
-import React from 'react';
-import Link from './Link';
+import React from "react";
+import Link from "./Link";
+import { Menu } from "lucide-react";
 
-const navegationData = 
-[
-    {
-      "id": 1,
-      "name": "Home",
-      "path": "/home"
-    },
-    {
-      "id": 2,
-      "name": "Books",
-      "path": "/books"
-    },
-    {
-      "id": 3,
-      "name": "Authors",
-      "path": "/authors"
-    },
-    {
-      "id": 4,
-      "name": "Categories",
-      "path": "/categories"
-    },
-    {
-      "id": 5,
-      "name": "Contact",
-      "path": "/contact"
-    }
-  ];
+
+const navegationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/home",
+  },
+  {
+    id: 2,
+    name: "Books",
+    path: "/books",
+  },
+  {
+    id: 3,
+    name: "Authors",
+    path: "/authors",
+  },
+  {
+    id: 4,
+    name: "Categories",
+    path: "/categories",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
 
 const Navber = () => {
-    return (
-        <nav>
+  return (
+    <nav className="flex justify-between mx-10">
+        <Menu></Menu>
+      <h3>My Navber</h3>
 
-            <ul className='flex'>
-                {
-                    navegationData.map(route => <Link route = {route}></Link>)
-                }
-            </ul>
+      <ul className="flex">
+        {navegationData.map((route) => (
+          <Link route={route}></Link>
+        ))}
+      </ul>
 
-            {/* <ul className='flex'>
+      {/* <ul className='flex'>
                 {
                     navegationData.map(route => 
                     <li className='mr-10'>
@@ -51,15 +54,16 @@ const Navber = () => {
                 )}
             </ul> */}
 
-
-            {/* <ul className='flex'>
+      {/* <ul className='flex'>
                 <li className='mr-10'><a href="#">Home</a></li>
                 <li className='mr-10'><a href="#">Blog</a></li>
                 <li className='mr-10'><a href="#">About</a></li>
                 <li className='mr-10'><a href="#">Link</a></li>
             </ul> */}
-        </nav>
-    );
+
+      <button>Sign In</button>
+    </nav>
+  );
 };
 
 export default Navber;
